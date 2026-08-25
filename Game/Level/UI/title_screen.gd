dc7b01dd9845f8a3b8f08d273e7fb4492d1c1769
Josh Clemens <josh.clemens@outlook.com>
2026-08-25 14:47:19 -0700

    Add How to Play panel to title screen with controls/abilities/combo info

--- Game/Level/UI/title_screen.gd
+++ Game/Level/UI/title_screen.gd
@@ -5,6 +5,7 @@ extends Node3D
 @onready var title_panel: Control = $Title_Options/Title
 @onready var menu_panel: Control = $Title_Options/Menu
 @onready var credit_panel: Control = $Title_Options/CreditPanel
+@onready var how_to_play_panel: Control = $Title_Options/HowToPlayPanel
 
 func _ready() -> void:
 	if level_loader.get_child(0).name == "TitleScreen":
@@ -18,9 +19,16 @@ func _on_options_button_pressed() -> void:
 	options_panel.visible = true
 	menu_panel.visible = false
 	credit_panel.visible = false
+	how_to_play_panel.visible = false
 	options_panel.get_node("HBoxContainer/PanelContainer/VBoxContainer/back").grab_focus()
 	#print("testing options_button_pressed")
 
 func _on_credits_button_pressed() -> void:
 	credit_panel.visible = true
+	how_to_play_panel.visible = false
 	#print("testing credits_button_pressed")
+
+func _on_how_to_play_pressed() -> void:
+	how_to_play_panel.visible = true
+	credit_panel.visible = false
+	#print("testing how_to_play_pressed")
